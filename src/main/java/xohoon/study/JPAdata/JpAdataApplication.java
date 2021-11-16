7fbca4d11af9f999fdfba907e00cda637d84d28a
xohoon <xohoonx2@gmail.com>
2021-11-17 00:10:47 +0900

    untracked file update

--- src/main/java/xohoon/study/JPAdata/JpAdataApplication.java
+++ src/main/java/xohoon/study/JPAdata/JpAdataApplication.java
@@ -2,8 +2,10 @@ package xohoon.study.JPAdata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+// @EnableJpaRepositories(basePackages = "") -> spring boot는 없어도 됨
 public class JpAdataApplication {
 
 	public static void main(String[] args) {
