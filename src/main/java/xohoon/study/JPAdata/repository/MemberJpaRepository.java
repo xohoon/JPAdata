7be817f4d4112ac0cd7774ea6d657fe64648b24a
xohoon <xohoonx2@gmail.com>
2021-11-16 22:33:19 +0900

    test commit

---
+++ src/main/java/xohoon/study/JPAdata/repository/MemberJpaRepository.java
@@ -0,0 +1,23 @@
+package xohoon.study.JPAdata.repository;
+
+import org.springframework.stereotype.Repository;
+import xohoon.study.JPAdata.entity.Member;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
---
+++ src/test/java/xohoon/study/JPAdata/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,27 @@
+package xohoon.study.JPAdata.repository;
+
+import static org.assertj.core.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import xohoon.study.JPAdata.entity.Member;
+
+// @RunWith(SpringRunner.class) junit5 + spring boot 에서 안써도 됨
+@SpringBootTest
+@Transactional
+class MemberJpaRepositoryTest {
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+    }
+}
