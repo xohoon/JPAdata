e178b6f67870fc8aa97ce1176727ed4f05622027
xohoon <xohoonx2@gmail.com>
2021-11-16 23:22:37 +0900

    team entity and test

--- src/main/java/xohoon/study/JPAdata/entity/Member.java
+++ src/main/java/xohoon/study/JPAdata/entity/Member.java
@@ -1,23 +1,39 @@
 package xohoon.study.JPAdata.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "username", "age"}) // team은 찍으면 안된다
 public class Member {
+
     @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
-    protected Member() {
-    }
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if (team != null) {
+            changeTeam(team);
+        }
+    }
+
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
     }
 }
