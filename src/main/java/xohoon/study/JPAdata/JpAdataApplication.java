33ba373944b7967f3578b1fae296756211ca88e4
xohoon <xohoonx2@gmail.com>
2021-11-16 22:14:16 +0900

    spring data JPA study project

---
+++ src/main/java/xohoon/study/JPAdata/JpAdataApplication.java
@@ -0,0 +1,13 @@
+package xohoon.study.JPAdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpAdataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpAdataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xohoon/study/JPAdata/JpAdataApplicationTests.java
@@ -0,0 +1,13 @@
+package xohoon.study.JPAdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpAdataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
