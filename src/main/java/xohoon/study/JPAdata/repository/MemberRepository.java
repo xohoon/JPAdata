bec2ba3784cdc56e561703ea8c19371f517abb97
xohoon <xohoonx2@gmail.com>
2021-11-17 02:17:59 +0900

    query method test

--- src/main/java/xohoon/study/JPAdata/repository/MemberRepository.java
+++ src/main/java/xohoon/study/JPAdata/repository/MemberRepository.java
@@ -4,5 +4,10 @@ package xohoon.study.JPAdata.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import xohoon.study.JPAdata.entity.Member;
 
+import java.util.List;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findTop3HelloBy();
 }
